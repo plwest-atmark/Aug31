1aa2f8204694628d252d03dfcb2251a8dcfe548e
seiji <seiji.atmark@gmail.com>
2019-09-03 17:59:26 +0900

    for practice

---
+++ Seiji_intro/Seiji_intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seiji_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
